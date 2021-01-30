cc6636a169cbaabc8e756e2b106cf6afa56bbb0d
Marcelo Wieja <marcelo.wieja@nubicall.com>
2021-01-30 13:52:58 -0300

    Initial commit

---
+++ src/main/java/com/melitest/fraudcontext/FraudContextApplication.java
@@ -0,0 +1,13 @@
+package com.melitest.fraudcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudContextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melitest/fraudcontext/FraudContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melitest.fraudcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
